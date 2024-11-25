fa5aa8d3beb9e9d9ee457763f2994a99e6498e62
bendarte <osman.stockholm@gmail.com>
2024-11-25 20:38:54 +0100

    feat: add component exports

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { Card } from './components/card';
+export { SearchBar } from './components/searchbar';
+export { Input } from './components/input';
+export { Button } from './components/button';
