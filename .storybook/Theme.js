e17af022a34821b0b109924ada17367c93ff6284
bendarte <osman.stockholm@gmail.com>
2024-11-25 20:37:11 +0100

    chore: add storybook configuration

---
+++ .storybook/Theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'My custom Storybook',
+  brandUrl: 'https://example.com',
+  brandImage: 'https://ui-avatars.com/api/?name=Component+Library&size=150&background=0D8ABC&color=fff',
+  brandTarget: '_self',
+});
